1b99d51749b691ae9dd901513a1cadfa7c9e4081
Erhan Tezcan <erhany96@gmail.com>
2022-05-08 13:20:27 +0300

    fix

--- utils/index.ts
+++ utils/index.ts
@@ -3,13 +3,13 @@ export const isTodayMothersDay = (): boolean => {
   // get today; it should be May & Sunday
   const today: Date = new Date()
   const todayWeekday: string = today.toLocaleDateString("en-US", { weekday: "long" })
-  if (todayWeekday == "Sunday" && today.getMonth() == 5) {
+  if (todayWeekday == "Sunday" && today.getMonth() == 4) {
     // get today last week; it should be in May
     const prev: Date = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000) // - 7 days
-    if (prev.getMonth() == 5) {
+    if (prev.getMonth() == 4) {
       //get today the previous week; it should be in April
       const prevprev: Date = new Date(prev.getTime() - 7 * 24 * 60 * 60 * 1000) // - 14 days
-      if (prevprev.getMonth() == 4) {
+      if (prevprev.getMonth() == 3) {
         return true
       }
     }
